bb85c65f618daca445d42350e98edb89e15be437
Adrián Alonso Gonzalo <idriskameni@gmail.com>
2023-06-25 18:46:01 +0200

    moved script to script.js file only

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var divElement = document.getElementById('viz1687709115691');
 var vizElement = divElement.getElementsByTagName('object')[0];
 
 if (divElement.offsetWidth > 800) {
-  vizElement.style.width = '1300px';
-  vizElement.style.height = '877px';
-} else if (divElement.offsetWidth > 500) {
-  vizElement.style.width = '1300px';
-  vizElement.style.height = '877px';
-} else {
   vizElement.style.width = '100%';
-  vizElement.style.height = '1977px';
+  vizElement.style.height = '100%';
+} else if (divElement.offsetWidth > 500) {
+  vizElement.style.width = '100%';
+  vizElement.style.height = '100%';
+} else {
+  vizElement.style.width = '1375px';
+  vizElement.style.height = '900px';
 }
 
 var scriptElement = document.createElement('script');
